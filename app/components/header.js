ab16353fb2b438a7e7a45c9fb4cfbefaf1fdfefc
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-25 09:53:21 +0300

    style: add header component

---
+++ app/components/header.js
@@ -0,0 +1,8 @@
+export const header = () => {
+    return (
+        <>
+            <img src="/logo.png" alt="A server sorrounded by magic sparckles" />
+            <h1>Welcom to this NextJS Course</h1>
+        </>
+    )
+}
